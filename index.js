a2d5857d59ac19c1419d26f3018b04f1f4f5c0b2
Rafael Miziara <rafaelmiziara@hotmail.com>
2020-09-08 23:29:18 +0200

    Fixing Heroku Deploy

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ app.use(express.static('public'));
 const router = require('./src/routes');
 app.use(router);
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000!');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}!`);
 });
