de8e2f6636b6aedf6bf8a30fca46f5c8f6f93306
Rafael Miziara <rafaelmiziara@hotmail.com>
2020-09-08 19:00:52 +0200

    Updating express

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.use(express.static('public'));
 
-const router = require('./routes');
+const router = require('./src/routes');
 app.use(router);
 
 app.listen(3000, () => {
