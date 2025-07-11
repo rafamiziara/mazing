7e1c0e20ccc19d42b381756e70509e0ee8953bee
Rafael Miziara <rafaelmiziara@hotmail.com>
2025-07-11 18:20:25 +0200

    Show stage & level

--- src/app/game/game.tsx
+++ src/app/game/game.tsx
@@ -1,6 +1,8 @@
 'use client'
 import { useMaze, useTimer } from '@/hooks'
+import { getLevel } from '@/utils'
 import dayjs from 'dayjs'
+import { Pause, Play, RotateCcw } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { useCallback, useEffect, useRef } from 'react'
@@ -42,23 +44,19 @@ export default function Game({ stage }: Props) {
   return (
     <>
       <div className="flex justify-between place-items-center px-4 h-20 font-bold font-[family-name:var(--font-fredoka)]">
-        <Link key="home" href="/" className="bg-slate-600 px-3 py-1.5 rounded-md">
+        <Link key="home" href="/" className="bg-slate-600 px-3 py-1.5 rounded-md z-20">
           mazing
         </Link>
-        <div className="flex justify-end">
+        <div className="fixed top-5 right-0 left-0  flex justify-center items-center">
+          <div className="bg-cyan-500 rounded-lg p-2 mr-4">{getLevel(stage).toLocaleUpperCase()}</div>
+          <div>STAGE: {stage}</div>
+        </div>
+        <div className="flex justify-end z-20">
           {(isRunning || isPaused) && (
-            <>
-              <Image onClick={clearStage} className="cursor-pointer mr-8" src="/refresh.png" alt="restart button" width={40} height={40} />
-              <Image
-                onClick={isRunning ? pauseGame : playGame}
-                className="cursor-pointer mr-8"
-                src={isRunning ? '/pause.png' : '/play.png'}
-                alt={isRunning ? 'pause button' : 'play_button'}
-                width={40}
-                height={40}
-              />
-            </>
+            <RotateCcw className="p-2 bg-cyan-500 rounded-lg mr-4 cursor-pointer" onClick={clearStage} size={40} />
           )}
+          {isRunning && <Pause className="p-2 bg-amber-400 rounded-lg mr-8 cursor-pointer" onClick={pauseGame} size={40} />}
+          {isPaused && <Play className="p-2 bg-emerald-500 rounded-lg mr-8 cursor-pointer" onClick={playGame} size={40} />}
           <div className="bg-slate-600 min-w-20 text-center py-1.5 rounded-md">{dayjs(time).format('mm:ss')}</div>
         </div>
       </div>
